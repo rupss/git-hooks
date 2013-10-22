d81e087b5f86048a46405723d5a08c1b49aef284
Rupa Shankar <rupsshankar@gmail.com>
2013-10-22 15:48:50 -0400

    Done. Approach is to take all the modified files from the commit, replace tabs with 4 spaces, add those files on git, and then commit --amend with the old commit message.
    However, this isn't good if you have uncommitted hunks in your files.

--- post-commit.py
+++ post-commit.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python
 
+import subprocess
+
+def execute_git_command(args):
+	pr = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	return pr.communicate()
+
+def main():
+	(commit_id, error) = execute_git_command(['git', 'rev-parse', 'HEAD'])
+	(files, error) = execute_git_command(['git', 'diff-tree', '--no-commit-id', '--name-only', '-r', commit_id])
+	print files
+
+if __name__ == "__main__":
+	main()
